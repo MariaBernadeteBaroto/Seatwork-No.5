4b75dfe92d5ccd9f4d65b0a6bb84b0d10f13fea1
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-12-10 11:09:57 +0800

    Importing and Initializing deque

---
+++ Queue.py
@@ -0,0 +1,13 @@
+# Building Queue Data Type
+
+# Custom FIFO queue to support at least the enqueue and dequeue operations
+# Write a bare-bones Queue class that will delegate those two operations.
+
+from collections import deque
+
+class Queue:
+    def _init_(self):
+        self._elements = deque()
+        
+# enqueue.append() method
+# dequeue.append() method

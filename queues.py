d07b121caa7f8079f681a9344be48f3abbf3a3cf
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-12-11 21:05:46 +0800

    Initialize iter and len methods

--- queues.py
+++ queues.py
@@ -3,12 +3,23 @@
 # Custom FIFO queue to support at least the enqueue and dequeue operations
 # Write a bare-bones Queue class that will delegate those two operations.
 
+
 from collections import deque
 
+# Make the class iterable (report its length and optionally accepts initial elements)
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    def __init__(self, elements):
+        self._elements = deque(elements)
 
+    #report length
+    def __len__(self):
+        return len(self._elements)
+
+    #make it iretable
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+            
     # enqueue.append() method
     def enqueue(self, element):
         self._elements.append(element)

33852c7a26d46b8d884364f41e5dd941132ca14f
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-12-15 20:28:06 +0800

    implemented Priority Queue

--- queue-test.py
+++ queue-test.py
@@ -3,6 +3,7 @@
 
 from queues import Queue
 from queues import Stack
+from queues import PriorityQueue
 
 #Implementation of __iter__ and __len__ methods;
 
@@ -33,3 +34,17 @@ from heapq import heappop
 heappop(fruits)
 
 print(fruits)
+
+#-------------------------------
+#Priority Queue Data Type
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT,"windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())

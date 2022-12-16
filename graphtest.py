8044f4c68236bb9e56634a6f144f101c476571a7
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-12-16 11:50:58 +0800

    implementing graph.py

--- graphtest.py
+++ graphtest.py
@@ -3,3 +3,8 @@ from graph import City, load_graph
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 print(nodes["london"])
+
+print(graph)
+
+#Could not download pygraphviz
+#Pygraphviz requires c/c++ compiler

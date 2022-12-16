5a5efa5d83b3dbbd0398ce48df73a7d4eceea18e
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-12-16 11:22:19 +0800

    added dataclass, heapify, heapify & MutableMinHeap

--- queues.py
+++ queues.py
@@ -5,9 +5,31 @@
 
 
 from collections import deque
-from heapq import heappop, heappush # Building Priority Queue Data Type
+from dataclasses import dataclass
+from heapq import heapify,heappop, heappush # Building Priority Queue Data Type
 from itertools import count 
+from typing import Any
 
+
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+        
 # Make the class iterable (report its length and optionally accepts initial elements)
 class Queue:
     def __init__(self, *elements):
@@ -57,3 +79,32 @@ class PriorityQueue(IterableMixin):
 
     def dequeue(self):
         return heappop(self._elements)[-1]
+
+@dataclass(order=True)
+class Element:
+    priority: float
+    count: int
+    value: Any
+
+
+class MutableMinHeap(IterableMixin):
+    def __init__(self):
+        super().__init__()
+        self._elements_by_value = {}
+        self._elements = []
+        self._counter = count()
+
+    def __setitem__(self, unique_value, priority):
+        if unique_value in self._elements_by_value:
+            self._elements_by_value[unique_value].priority = priority
+            heapify(self._elements)
+        else:
+            element = Element(priority, next(self._counter), unique_value)
+            self._elements_by_value[unique_value] = element
+            heappush(self._elements, element)
+
+    def __getitem__(self, unique_value):
+        return self._elements_by_value[unique_value].priority
+
+    def dequeue(self):
+        return heappop(self._elements).value
